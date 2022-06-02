021a06df2758de1fe66ade8e61922d4b3bdbe162
livgraf <livio.graf@lernende.ethz.ch>
2022-06-02 16:08:00 +0200

    first commit

---
+++ src/main/java/ch/bbw/modul_431_todo_application/Modul431TodoApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.modul_431_todo_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modul431TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modul431TodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/modul_431_todo_application/Modul431TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.modul_431_todo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modul431TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
