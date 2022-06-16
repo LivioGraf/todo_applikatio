a23ace5a747fce40bf9e62f4cbaa8e00eed31ab3
livgraf <livio.graf@lernende.ethz.ch>
2022-06-16 16:15:49 +0200

    - Alle Routenen gemacht

---
+++ src/main/java/ch/bbw/modul_431_todo_application/todoController.java
@@ -0,0 +1,49 @@
+package ch.bbw.modul_431_todo_application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class todoController {
+
+/* Routen um alle Todos zu sehen */
+    @GetMapping("/")
+    public String getTodos() {
+        return "todo";
+    }
+
+/* Routen um ein Todo zu erstellen */
+
+    @GetMapping("/createtodo")
+    public String getCreateTodos() {
+        return "createtodo";
+    }
+
+    @PostMapping("/createtodo")
+    public String postCreateTodos() {
+        return "createtodo";
+    }
+
+/* Routen um ein Todo zu bearbeiten */
+    @GetMapping("/updatetodo")
+    public String getUpdateTodos() {
+        return "updatetodo";
+    }
+
+    @PostMapping("/updatetodo")
+    public String postUpdateTodos() {
+        return "updatetodo";
+    }
+
+/* Routen um ein Todo zu löschen*/
+    @GetMapping("/deletetodo")
+    public String getDeleteTodos() {
+        return "deletetodos";
+    }
+
+    @PostMapping("/deletetodo")
+    public String postDeleteTodos() {
+        return "deletetodos";
+    }
+}
