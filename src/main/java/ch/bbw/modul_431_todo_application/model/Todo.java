9ff77e5d5737fa3dad6ecbf8bf1625eb6deb50fd
livgraf <livio.graf@lernende.ethz.ch>
2022-06-23 21:58:18 +0200

    -

--- src/main/java/ch/bbw/modul_431_todo_application/model/Todo.java
+++ src/main/java/ch/bbw/modul_431_todo_application/model/Todo.java
@@ -1,13 +1,40 @@
 package ch.bbw.modul_431_todo_application.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "Todo")
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "todoName")
+    private String todoName;
+
+    public Todo() {
+
+    }
+
+    public Todo(String todoName) {
+        this.todoName = todoName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTodoName() {
+        return todoName;
+    }
+
+    public void setTodoName(String todoName) {
+        this.todoName = todoName;
+    }
+
 }
